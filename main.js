ff8473be47e659dbd91c9dc338d5287dc8e99b8c
salvatorebriga <salvo.z2302@gmail.com>
2024-04-24 18:12:11 +0200

    final commit

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 'use strict'
 
 const everyKm = 0.21;
-let kilometers = prompt();
-let age = prompt();
+let kilometers = prompt('Inserisci i kilometri');
+let age = prompt("inserisci l'età");
 
 let price = kilometers * everyKm;
+console.log(price.toFixed(2));
 
-if(age < 18){
-    price -= (price * 20) / 100;
-}else if(age > 65){
-    price -= (price * 40) / 100;
+if(age < 18 && age != 0){
+    price = price - ((price * 20) / 100);
+    document.getElementById('prezzo').innerHTML = price.toFixed(2) + '$' + ' sconto giovani del 20%';
+}else if(age > 65 && age < 110){
+    price = price - ((price * 40) / 100);
+    document.getElementById('prezzo').innerHTML = price.toFixed(2) + '$' + ' sconto over 65 del 40%';
+}
+else{
+    document.getElementById('prezzo').innerHTML = price.toFixed(2) + '$';
 }
 
-document.getElementById('prezzo').innerHTML = price.toFixed(2) + '$';
 console.log(price.toFixed(2));
