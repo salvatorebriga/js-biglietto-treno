7c76045e4fec4a86d14a82b7c45ae7e555396020
salvatorebriga <salvo.z2302@gmail.com>
2024-04-24 17:16:23 +0200

    added travel cost first total calculation

--- main.js
+++ main.js
@@ -3,3 +3,8 @@
 const everyKm = 0.21;
 let kilometers = prompt();
 let age = prompt();
+
+let price = kilometers * everyKm;
+
+document.getElementById('prezzo').innerHTML = price + '$';
+console.log(price);
